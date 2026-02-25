0230e4f606b2561449cfcad0eac629bf1a73d502
Carsen <carse.astro@gmail.com>
2026-02-24 18:12:15 -0700

    Added reveal animation, template components, and barrel importing, profile sticky.

---
+++ src/components/sections/index.ts
@@ -0,0 +1,6 @@
+export { default as Hero } from "./Hero.astro";
+export { default as RecentProjects } from "./RecentProjects.astro";
+export { default as Experience } from "./Experience.astro";
+export { default as Tools } from "./Tools.astro";
+export { default as Blog } from "./Blog.astro";
+export { default as Contact } from "./Contact.astro";
